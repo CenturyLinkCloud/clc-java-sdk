d744921d6a57f626c9fef2f42802fc3edde34717
Ilya Drabenia <ilya.drabenia@altoros.com>
2015-03-02 16:25:36 +0300

    Create clc-java-sdk initial skeleton

---
+++ clc-java-sdk/src/main/java/CreateServerResult.java
@@ -0,0 +1,26 @@
+
+public class CreateServerResult {
+
+
+    private String result;
+
+
+
+    /**
+     * Operation Result
+     */
+    public CreateServerResult result(String result) {
+        this.result = result;
+        return this;
+    }
+
+
+
+    /**
+     * Operation Result
+     */
+    public String getResult() {
+        return result;
+    }
+
+}
---
+++ clc-java-sdk/src/main/java/TestService.java
@@ -0,0 +1,21 @@
+/**
+ * @author ilya.drabenia
+ */
+public class TestService {
+
+    public static void main(String... args) {
+        System.out.println(
+            new ServerService()
+                .create(new CreateServerCommand()
+                    .cpu(10)
+                    .groupId("A5")
+                    .memoryGB(10)
+                    .name("server1")
+                    .type(5)
+                    .sourceServerId("server0")
+                )
+                .getResult()
+        );
+    }
+
+}
