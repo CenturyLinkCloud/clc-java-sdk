a91b502d2d79e702b4b9bdbc8b2ae1b518dd1374
Ilya Drabenia <ilya.drabenia@altoros.com>
2015-03-09 20:12:34 +0300

    39-sample-application-integrate-required-frontend-technologies-and-create-sample-application-skeleton

---
+++ clc-java-sdk/sample/client/src/jsx/components/body.jsx
@@ -0,0 +1,44 @@
+
+let { RouteHandler } = ReactRouter;
+
+
+export class Body extends React.Component {
+
+    constructor (props) {
+        super(props);
+        this.state = { loading: true };
+    }
+
+    render () {
+        return (
+            <div>
+                <nav className="navbar navbar-inverse navbar-fixed-top">
+                    <div className="container">
+                        <div className="navbar-header">
+                            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse"
+                                data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                                <span className="sr-only">Toggle navigation</span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                            </button>
+                            <a className="navbar-brand" href="#">Project name</a>
+                        </div>
+                        <div id="navbar" className="collapse navbar-collapse">
+                            <ul className="nav navbar-nav">
+                                <li className="active"><a href="#">Home</a></li>
+                                <li><a href="#about">About</a></li>
+                                <li><a href="#contact">Contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+
+                <div className="container">
+                    <RouteHandler />
+                </div>
+            </div>
+        );
+    }
+
+}
