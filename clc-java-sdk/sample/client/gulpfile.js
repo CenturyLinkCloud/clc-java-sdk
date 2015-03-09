086dc8e2be8e37b12e0f8ef8095792719d54daeb
Ilya Drabenia <ilya.drabenia@altoros.com>
2015-03-09 15:17:09 +0300

    39-sample-application-integrate-required-frontend-technologies-and-create-sample-application-skeleton

--- clc-java-sdk/sample/client/gulpfile.js
+++ clc-java-sdk/sample/client/gulpfile.js
@@ -9,29 +9,28 @@ var reactify = require('reactify');
 var streamify = require('gulp-streamify');
 var sass = require('gulp-sass');
 
+var toDist = gulp.dest('dist');
+var toAppStatic = gulp.dest('../app/src/main/resources/static');
 
 gulp.task('buildJs', function () {
-    browserify({
-        entries: ['./src/jsx/app.jsx'],
-        transform: [reactify]
-    })
-    .bundle()
-    .pipe(source('app.js'))
-    .pipe(gulp.dest('dist'))
-    .pipe(gulp.dest('../app/src/main/resources/static'));
+    browserify({ entries: ['./src/jsx/app.jsx'], transform: [reactify]})
+        .bundle()
+        .pipe(source('app.js'))
+        .pipe(toDist)
+        .pipe(toAppStatic);
 });
 
 gulp.task('buildCss', function () {
     gulp.src('src/scss/sample.scss')
         .pipe(sass())
-        .pipe(gulp.dest('dist'))
-        .pipe(gulp.dest('../app/src/main/resources/static'));
+        .pipe(toDist)
+        .pipe(toAppStatic);
 });
 
 gulp.task('buildHtml', function () {
     gulp.src('src/index.html')
-        .pipe(gulp.dest('dist'))
-        .pipe(gulp.dest('../app/src/main/resources/static'));
+        .pipe(toDist)
+        .pipe(toAppStatic);
 });
 
 gulp.task('default', ['buildJs', 'buildCss', 'buildHtml']);
