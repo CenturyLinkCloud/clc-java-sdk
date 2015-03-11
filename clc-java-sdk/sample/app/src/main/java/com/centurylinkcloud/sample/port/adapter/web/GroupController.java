94cc89e1f53583ce81e23586813d838ef3a3c90d
Ilya Drabenia <ilya.drabenia@altoros.com>
2015-03-11 16:20:16 +0300

    47-sample-application-implement-create-server-operation

--- clc-java-sdk/sample/app/src/main/java/com/centurylinkcloud/sample/port/adapter/web/GroupController.java
+++ clc-java-sdk/sample/app/src/main/java/com/centurylinkcloud/sample/port/adapter/web/GroupController.java
@@ -2,10 +2,11 @@ package com.centurylinkcloud.sample.port.adapter.web;
 
 import com.centurylinkcloud.ClcSdk;
 import com.centurylinkcloud.core.auth.domain.credentials.StaticCredentialsProvider;
+import com.centurylinkcloud.sample.application.SdkRegistry;
 import com.centurylinkcloud.servers.domain.Group;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,10 +20,13 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @RequestMapping("/datacenter/{dataCenter}/group")
 public class GroupController {
 
+    @Autowired
+    SdkRegistry sdkRegistry;
+
     @RequestMapping(method = GET)
     public List<Group> findAll(@PathVariable("dataCenter") String dataCenter) {
         return
-            new ClcSdk(new StaticCredentialsProvider("idrabenia", "RenVortEr9"))
+            sdkRegistry.findOrCreate("idrabenia", "RenVortEr9")
                 .groupService()
                 .findByDataCenter(dataCenter);
     }
