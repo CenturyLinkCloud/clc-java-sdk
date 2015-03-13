57b80cf0a650140e83590ba1c59c4045356d496f
Ilya Drabenia <ilya.drabenia@altoros.com>
2015-03-13 17:20:26 +0300

    48-implement-unit-tests-for-existing-functionality

---
+++ clc-java-sdk/sdk/src/test/java/com/centurylink/cloud/sdk/core/auth/services/BearerAuthenticationTest.java
@@ -0,0 +1,37 @@
+package com.centurylink.cloud.sdk.core.auth.services;
+
+import com.centurylink.cloud.sdk.core.auth.client.LoginClient;
+import com.centurylink.cloud.sdk.core.auth.services.domain.credentials.StaticCredentialsProvider;
+import org.testng.annotations.Test;
+
+import javax.ws.rs.client.ClientRequestContext;
+import javax.ws.rs.core.MultivaluedHashMap;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class BearerAuthenticationTest {
+
+    BearerAuthentication auth = new BearerAuthentication(
+            new StaticCredentialsProvider("idrabenia", "RenVortEr9"),
+            new LoginClient()
+    );
+
+    private ClientRequestContext stubRequestContext() {
+        ClientRequestContext context = mock(ClientRequestContext.class);
+
+        when(context.getHeaders()).thenReturn(new MultivaluedHashMap<>());
+
+        return context;
+    }
+
+    @Test
+    public void testBearerTokenFilter() throws Exception {
+        ClientRequestContext context = stubRequestContext();
+
+        auth.filter(context);
+
+        assert context.getHeaders().get("Authorization") != null;
+    }
+
+}
