fb0c8f030fde1572ff75667192aaaf5b3035cea4
Ilya Drabenia <ilya.drabenia@altoros.com>
2015-03-13 16:05:19 +0300

    48-implement-unit-tests-for-existing-functionality

---
+++ clc-java-sdk/sdk/src/test/java/com/centurylink/cloud/sdk/core/auth/client/LoginClientTest.java
@@ -0,0 +1,27 @@
+package com.centurylink.cloud.sdk.core.auth.client;
+
+import com.centurylink.cloud.sdk.core.auth.client.domain.LoginRequest;
+import com.centurylink.cloud.sdk.core.auth.client.domain.LoginResponse;
+import com.google.inject.Inject;
+import org.testng.annotations.Test;
+
+import javax.ws.rs.client.ResponseProcessingException;
+
+public class LoginClientTest {
+
+    LoginClient client = new LoginClient();
+
+    @Test
+    public void testLoginWithCorrectCredentials() {
+        LoginResponse response = client.login(new LoginRequest("idrabenia", "RenVortEr9"));
+
+        assert response.getAccountAlias() != null;
+        assert response.getBearerToken() != null;
+    }
+
+    @Test(expectedExceptions = ResponseProcessingException.class)
+    public void testLoginWithIncorrectCredentials() {
+        client.login(new LoginRequest("incorrect", "account"));
+    }
+
+}
