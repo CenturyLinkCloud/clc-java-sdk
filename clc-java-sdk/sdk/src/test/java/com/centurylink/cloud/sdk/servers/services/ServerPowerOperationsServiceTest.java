2f7b9db9c2c61e24a95cd4da0d2d957765533bfe
Sergey Fedosenko <superpslam@gmail.com>
2015-04-16 11:59:44 +0300

    97 98 99 ServerPowerOperationsServiceTest implementation

--- clc-java-sdk/sdk/src/test/java/com/centurylink/cloud/sdk/servers/services/ServerPowerOperationsServiceTest.java
+++ clc-java-sdk/sdk/src/test/java/com/centurylink/cloud/sdk/servers/services/ServerPowerOperationsServiceTest.java
@@ -3,16 +3,13 @@ package com.centurylink.cloud.sdk.servers.services;
 import com.centurylink.cloud.sdk.servers.AbstractServersSdkTest;
 import com.centurylink.cloud.sdk.servers.client.domain.server.Details;
 import com.centurylink.cloud.sdk.servers.client.domain.server.metadata.ServerMetadata;
-import com.centurylink.cloud.sdk.servers.services.domain.server.Server;
 import com.centurylink.cloud.sdk.servers.services.domain.server.refs.ServerRef;
 import com.centurylink.cloud.sdk.tests.fixtures.SingleServerFixture;
 import com.google.inject.Inject;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 
 import static com.centurylink.cloud.sdk.tests.TestGroups.INTEGRATION;
 import static com.centurylink.cloud.sdk.tests.TestGroups.LONG_RUNNING;
-
 
 public class ServerPowerOperationsServiceTest extends AbstractServersSdkTest {
 
@@ -38,45 +35,85 @@ public class ServerPowerOperationsServiceTest extends AbstractServersSdkTest {
         assert loadServerDetails(server).getInMaintenanceMode().equals(expectedResult);
     }
 
-    public void testPowerOff() {
-        server = SingleServerFixture.server();
+    private void powerOnServer() {
+        serverService
+            .powerOn(server)
+            .waitUntilComplete();
+    }
 
+    private void powerOffServer() {
         serverService
             .powerOff(server)
             .waitUntilComplete();
+    }
+
+    private void pauseServer() {
+        serverService
+            .pause(server)
+            .waitUntilComplete();
+    }
+
+    private void shutDownServer() {
+        serverService
+            .shutDown(server)
+            .waitUntilComplete();
+    }
+
+    private void stopServerMaintenance() {
+        serverService
+            .stopMaintenance(server)
+            .waitUntilComplete();
+    }
+
+    private void startServerMaintenance() {
+        serverService
+            .startMaintenance(server)
+            .waitUntilComplete();
+    }
+
+    public void testPowerOff() {
+        testShutDown();
+        powerOffServer();
 
         assertThatPowerStateHasStatus(server, "stopped");
     }
 
+    public void testPause() {
+        pauseServer();
+
+        assertThatPowerStateHasStatus(server, "paused");
+        powerOnServer();
+    }
+
+    public void testShutDown() {
+        testPause();
+        shutDownServer();
+
+        assertThatPowerStateHasStatus(server, "stopped");
+        powerOnServer();
+    }
+
     public void testPowerOn() {
         testPowerOff();
-
-        serverService
-            .powerOn(server)
-            .waitUntilComplete();
+        powerOnServer();
 
         assertThatPowerStateHasStatus(server, "started");
     }
 
     public void testStartMaintenance() throws Exception {
         testPowerOn();
-
-        serverService
-            .startMaintenance(server)
-            .waitUntilComplete();
+        startServerMaintenance();
 
         assertThatMaintenanceFlagIs(server, true);
     }
 
     @Test(groups = {INTEGRATION, LONG_RUNNING})
     public void testStopMaintenance() throws Exception {
-        testStartMaintenance();
+        server = SingleServerFixture.server();
 
-        serverService
-            .stopMaintenance(server)
-            .waitUntilComplete();
+        testStartMaintenance();
+        stopServerMaintenance();
 
         assertThatMaintenanceFlagIs(server, false);
     }
-
 }
