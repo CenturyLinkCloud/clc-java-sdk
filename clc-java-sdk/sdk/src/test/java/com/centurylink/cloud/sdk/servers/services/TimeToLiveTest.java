fa8e88e40618ece26259b814a5d7c2a62c5e5051
Aliaksandr Krasitski <aliaksandr.krasitski@altoros.com>
2015-04-17 16:28:42 +0300

    82 Implement support of server TTL at create server operation

--- clc-java-sdk/sdk/src/test/java/com/centurylink/cloud/sdk/servers/services/TimeToLiveTest.java
+++ clc-java-sdk/sdk/src/test/java/com/centurylink/cloud/sdk/servers/services/TimeToLiveTest.java
@@ -5,27 +5,40 @@ import com.centurylink.cloud.sdk.servers.services.domain.server.TimeToLiveParseE
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.text.SimpleDateFormat;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * @author Aliaksandr Krasitski
  */
-public class TimeToLiveTest extends Assert{
+public class TimeToLiveTest{
+
+    private static final String TTL_PATTERN = "yyyy-MM-dd'T'HH:mmXXX";
+
+    private static final SimpleDateFormat DATE_TIME_FORMAT = new SimpleDateFormat(TTL_PATTERN);
 
     @Test
     public void testConstructorDate() {
-        new TimeToLive(Calendar.getInstance().getTime());
+        Date date = Calendar.getInstance().getTime();
+        TimeToLive ttl = new TimeToLive(date);
+        Assert.assertEquals(ttl.format(), DATE_TIME_FORMAT.format(date));
     }
 
     @Test
     public void testConstructorCalendar() {
-        new TimeToLive(Calendar.getInstance());
+        Calendar calendar = Calendar.getInstance();
+        TimeToLive ttl = new TimeToLive(calendar);
+        Assert.assertEquals(ttl.format(), DATE_TIME_FORMAT.format(calendar.getTime()));
     }
 
     @Test
     public void testConstructorZonedDateTime() {
-        new TimeToLive(ZonedDateTime.now());
+        ZonedDateTime dt = ZonedDateTime.now();
+        TimeToLive ttl = new TimeToLive(dt);
+        Assert.assertEquals(ttl.format(), dt.format(DateTimeFormatter.ofPattern(TTL_PATTERN)));
     }
 
     @Test(expectedExceptions = TimeToLiveParseException.class)
@@ -41,12 +54,17 @@ public class TimeToLiveTest extends Assert{
     @Test
     public void testConstructorString() {
         String date = "2015-12-31T23:00+03:00";
-        TimeToLive ttl = new TimeToLive("2015-12-31T23:00+03:00");
-        assertEquals(date, ttl.format());
+        TimeToLive ttl = new TimeToLive(date);
+        Assert.assertEquals(date, ttl.format());
     }
 
     @Test
     public void testConstructorStringPattern() {
-        new TimeToLive("2015-12-31T23:00+03:00", "yyyy-MM-dd'T'HH:mmXXX");
+        String date = "2015/12/31T23:00+03:00";
+        String pattern = "yyyy/MM/dd'T'HH:mmXXX";
+        TimeToLive ttl = new TimeToLive(date, pattern);
+        Assert.assertEquals(ttl.format(),
+                ZonedDateTime.parse(date, DateTimeFormatter.ofPattern(pattern))
+                        .format(DateTimeFormatter.ofPattern(TTL_PATTERN)));
     }
 }
