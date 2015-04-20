56fd1e28165cd6c9cd2aa5a6509f5e2d0df5841c
Aliaksandr Krasitski <aliaksandr.krasitski@altoros.com>
2015-04-20 16:14:45 +0300

    112 Implement possibilities to remove public IP address

--- clc-java-sdk/sdk/src/main/java/com/centurylink/cloud/sdk/servers/services/domain/ip/SourceRestriction.java
+++ clc-java-sdk/sdk/src/main/java/com/centurylink/cloud/sdk/servers/services/domain/ip/SourceRestriction.java
@@ -9,6 +9,16 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 public class SourceRestriction {
     private String cidr;
 
+    public String getCidr() {
+        return cidr;
+    }
+
+    public void setCidr(String cidr) {
+        this.cidr = cidr;
+    }
+
+    public SourceRestriction() {}
+
     public SourceRestriction(String cidr) {
         this.cidr = cidr;
     }
--- clc-java-sdk/sdk/src/test/java/com/centurylink/cloud/sdk/servers/services/PublicIpTest.java
+++ clc-java-sdk/sdk/src/test/java/com/centurylink/cloud/sdk/servers/services/PublicIpTest.java
@@ -6,15 +6,12 @@ import com.centurylink.cloud.sdk.servers.client.domain.server.IpAddress;
 import com.centurylink.cloud.sdk.servers.client.domain.server.PublicIpAddressResponse;
 import com.centurylink.cloud.sdk.servers.client.domain.server.metadata.ServerMetadata;
 import com.centurylink.cloud.sdk.servers.services.domain.ip.PortConfig;
-import com.centurylink.cloud.sdk.servers.services.domain.ip.ProtocolType;
 import com.centurylink.cloud.sdk.servers.services.domain.ip.PublicIpAddressRequest;
-import com.centurylink.cloud.sdk.servers.services.domain.ip.SourceRestriction;
 import com.centurylink.cloud.sdk.servers.services.domain.server.refs.ServerRef;
 import com.centurylink.cloud.sdk.tests.fixtures.SingleServerFixture;
 import com.google.inject.Inject;
 import org.testng.annotations.Test;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static com.centurylink.cloud.sdk.tests.TestGroups.INTEGRATION;
@@ -32,6 +29,8 @@ public class PublicIpTest extends AbstractServersSdkTest {
 
     @Test
     public void testPublicIpTest() {
+        SingleServerFixture f = new SingleServerFixture();
+        f.createServer();
         ServerRef serverRef = SingleServerFixture.server();
 
         Link response = serverService
@@ -53,12 +52,14 @@ public class PublicIpTest extends AbstractServersSdkTest {
         ipAddresses.parallelStream().forEach(address -> {
             if (address.getPublicIp() != null) {
                 PublicIpAddressResponse resp = serverService.getPublicIp(server.getId(), address.getPublicIp());
-                serverService.removePublicIp(server.getId(), address.getPublicIp());
+                serverService.removePublicIp(server.getId(), address.getPublicIp()).waitUntilComplete();
             }
         });
 
         List<IpAddress> initialIpAddresses = serverService.findByRef(serverRef).getDetails().getIpAddresses();
-        assertNotSame(ipAddresses.size(), initialIpAddresses.size());
+
+        assertEquals(ipAddresses.stream().filter(addr -> addr.getPublicIp() != null).count(),
+                initialIpAddresses.stream().filter(addr -> addr.getPublicIp() != null).count() + 1);
     }
 
 }
